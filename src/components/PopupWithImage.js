f4f2a726872d2f5d9fbb8dd690bfc82447964bb0
richard wetterauer <you@example.com>
2022-10-08 07:53:41 -0500

    fix CODE 23

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -4,12 +4,15 @@ export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
     this.image = this._popupElement.querySelector(".image-popup__picture-zoom");
-  }
-  open({ link, name }) {
-    const imageName = this._popupElement.querySelector(
+    this.imageName = this._popupElement.querySelector(
       ".image-popup__picture-text"
     );
-    imageName.textContent = name;
+  }
+  open({ link, name }) {
+    // const imageName = this._popupElement.querySelector(
+    //   ".image-popup__picture-text"
+    // );
+    this.imageName.textContent = name;
 
     this.image.src = link;
     this.image.alt = `Photo of ${name}`;
